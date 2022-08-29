cd4661bc6244b12c8871f2ab3f6918c15739dd91
vectorKim3333 <sh.kim@madup.com>
2022-08-29 18:53:23 +0900

    feat: 테이블 header UI 진행중

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 import Table from 'custom-component'
 
 function App() {
@@ -7,22 +7,87 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Table
-          headers={[{
-            key: 'hello0',
-            title: 'world0',
-            menu: [{
-              value: '메뉴입니다.',
-              onClick: () => {}
-            }]
-          }, {
-            key: 'hello1',
-            title: 'world1'
-          }]}
-          data={["fadsfs"]}
+          headers={[
+            {
+              key: 'hello0',
+              title: 'world0',
+              menu: [
+                {
+                  value: '메뉴입니다.',
+                  onClick: () => {
+                    console.log('메뉴클릭')
+                  },
+                },
+              ],
+            },
+            {
+              key: 'hello0',
+              title: 'world1',
+            },
+            {
+              key: 'hello0',
+              title: 'world2',
+              menu: [
+                {
+                  value: '메뉴입니다.',
+                  onClick: () => {
+                    console.log('메뉴클릭')
+                  },
+                },
+              ],
+            },
+            {
+              key: 'hello0',
+              title: 'world3',
+              menu: [
+                {
+                  value: '메뉴입니다.',
+                  onClick: () => {
+                    console.log('메뉴클릭')
+                  },
+                },
+              ],
+            },
+
+            {
+              key: 'hello1',
+              title: 'world4',
+              menu: [
+                {
+                  value: '메뉴입니다.2',
+                  onClick: () => {
+                    console.log('메뉴2클릭')
+                  },
+                },
+                {
+                  value: '메뉴입니다.3',
+                  onClick: () => {
+                    console.log('메뉴2클릭')
+                  },
+                },
+                {
+                  value: '메뉴입니다.3',
+                  onClick: () => {
+                    console.log('메뉴2클릭')
+                  },
+                },
+              ],
+            },
+          ]}
+          data={[
+            {
+              name: 'sss',
+              age: 22,
+            },
+            {
+              name: 'sdw',
+              age: 2231,
+            },
+          ]}
         />
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
