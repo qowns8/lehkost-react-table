11587eba247b36308d22397ae5d6fb872fefc27a
vectorKim3333 <sh.kim@madup.com>
2022-08-29 18:52:53 +0900

    feat: 테이블 Body 적용

---
+++ src/body/index.tsx
@@ -0,0 +1,11 @@
+import {ITableBodys} from "../types";
+
+export default function TableBody({ data }: ITableBodys) {
+  return  <div className="lehkost-table-body">
+    {
+      data.map(item => <div className="lehkost-table-body--tr">
+        {item.value}
+      </div>)
+    }
+  </div>
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,11 @@
 import './index.scss'
 import {ITableProps} from "./types";
 import Header from './header'
+import TableBody from "./body";
 
 export default function Component (props: ITableProps) {
   return <div className="lehkost-table-wrappper">
     <Header headers={props.headers}/>
-    <div className="lehkost-table-body">
-      {
-        props.data.map(item => <div className="lehkost-table-body--tr">
-          {item}
-        </div>)
-      }
-    </div>
+    <TableBody data={props.data}/>
   </div>
 }
